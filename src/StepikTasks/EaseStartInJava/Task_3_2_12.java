4e06076e7e47a75f50a2a9c719a74df99726b4be
ChaserPavel <Chaser1989@mail.ru>
2022-05-17 21:44:17 +0300

    Тест задачи на сайте Stepik принял решение! Учтены месяцы, дни, високосные года!

--- src/StepikTasks/EaseStartInJava/Task_3_2_12.java
+++ src/StepikTasks/EaseStartInJava/Task_3_2_12.java
@@ -26,98 +26,35 @@ public class Task_3_2_12 {
         int day = scan.nextInt();
         int month = scan.nextInt();
         int year = scan.nextInt();
-        if (year >= 1 && day >= 1 && day <= 31) {
-            switch (month) {
-                case 1:
-                    if (day <= 31) {
+
+        if (year >= 1 && day >= 1 && day <= 31 && month >= 1 && month <= 12) {
+            if (month == 2) {
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                    if (day == 29) {
                         System.out.println("true");
                     } else {
                         System.out.println("false");
                     }
-                    break;
-                case 2:
-                    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-                        if (day == 29) {
-                            System.out.println("true");
-                        }
-                    } else if (day <= 28) {
-                        System.out.println("true");
-                    }else {
-                        System.out.println("false");
-                    }
-                    break;
-                case 3:
-                    if (day <= 31) {
-                        System.out.println("true");
-                    } else {
-                        System.out.println("false");
-                    }
-                    break;
-                case 4:
-                    if (day <= 30) {
-                        System.out.println("true");
-                    } else {
-                        System.out.println("false");
-                    }
-                    break;
-                case 5:
-                    if (day <= 31) {
-                        System.out.println("true");
-                    } else {
-                        System.out.println("false");
-                    }
-                    break;
-                case 6:
-                    if (day <= 30) {
-                        System.out.println("true");
-                    } else {
-                        System.out.println("false");
-                    }
-                    break;
-                case 7:
-                    if (day <= 31) {
-                        System.out.println("true");
-                    } else {
-                        System.out.println("false");
-                    }
-                    break;
-                case 8:
-                    if (day <= 31) {
-                        System.out.println("true");
-                    } else {
-                        System.out.println("false");
-                    }
-                    break;
-                case 9:
-                    if (day <= 30) {
-                        System.out.println("true");
-                    } else {
-                        System.out.println("false");
-                    }
-                    break;
-                case 10:
-                    if (day <= 31) {
-                        System.out.println("true");
-                    } else {
-                        System.out.println("false");
-                    }
-                    break;
-                case 11:
-                    if (day <= 30) {
-                        System.out.println("true");
-                    } else {
-                        System.out.println("false");
-                    }
-                    break;
-                case 12:
-                    if (day <= 31) {
-                        System.out.println("true");
-                    } else {
-                        System.out.println("false");
-                    }
-                    break;
-                default:
+                }
+                else if (day <= 28){
+                    System.out.println("true");
+                }else{
                     System.out.println("false");
+                }
+            }
+            else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+                if(day <= 31){
+                    System.out.println("true");
+                }else{
+                    System.out.println("false");
+                }
+            }
+            else if (month == 4 || month == 6 || month == 9 || month == 11){
+                if (day <= 30){
+                    System.out.println("true");
+                }else{
+                    System.out.println("false");
+                }
             }
         } else {
             System.out.println("false");
